dd03bfe9b3fb9ca6dea71f49d7ed8bb02d856f6c
tukki0210 <tukki0210@gmail.com>
2023-01-26 14:09:23 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Vite Vue</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
